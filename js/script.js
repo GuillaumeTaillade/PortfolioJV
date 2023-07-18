6631c8f85cdf24954534c8fca837be897f48d3cd
GuillaumeTaillade <guigui.taillade@gmail.com>
2023-07-18 11:31:17 +0200

    ajout de l'index html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========== typing animation ========== */
+var typed = new Typed(".typing", {
+  strings:["","Game Developper","Web Developper"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+});
